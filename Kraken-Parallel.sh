6e1353bc6094b90ef9d2f6f4029b2ff63c655899
brunodupim08 <bruno152.bs@gmail.com>
2022-11-23 10:25:24 -0300

    log update and new options for log

--- Kraken-Parallel.sh
+++ Kraken-Parallel.sh
@@ -1,17 +1,28 @@
 #Read me
-#tmp_log must be informed before starting octopus.
-#script must be informed before starting octopus.
+#log_path must be informed before starting octopus.
+#new_log must be called before starting octopus
+#verbose must be informed before starting octopus.
 #fixed_command_input must be informed before starting octopus.
 #list_of_files_input must be informed before starting octopus.
 #limit_parallel must be informed before starting octopus.
 
-function log(){
-    name_file="banana"
+function create_log(){
+    export dir_log="$log_path/${file_input##*/}-op-log"
+    export log="${dir_log}/${file_input##*/}-line-${index_line}.log"
 
-    echo -e "++++++++++++++++++++++++++++\n$(date)\nFile: $file_input\nTotal lines: $total_lines\n++++++++++++++++++++++++++++\n" >> $tmp_log/$name_file.txt
+    #Tests if the log folder exists, if it doesn't exist it creates it.
+    if [[ ! -d "$dir_log" ]]; then
+        mkdir -p $dir_log
+    fi
+    #Test if new_log is true or false.
+    if [[ "$new_log" = true ]]; then
+        echo -e "[ $(date) | File: $file_input | Total lines: $total_lines ]\n" > $log
+    else
+        echo -e "[ $(date) | File: $file_input | Total lines: $total_lines ]\n" >> $log
+    fi
 }
 
-function progress_silent(){     #Display.
+function progress(){     #Display.
     echo -ne "\\r[ $total_lines / $index_line ][ Max: $limit_parallel | Actives: $active_parallel ]"
 }
 
@@ -24,12 +35,12 @@ function active_tentacles(){
 }
 
 function script_verbose(){      #Verbose mode.
-    ((index_line++))
-    echo -e "Line:$index_line\nCommand: $fixed_command_input $line_input\n$($fixed_command_input $line_input 2>&1)\n" | tee -a $tmp_log/$name_file.txt &
+    (task=$($fixed_command_input $line_input 2>&1)
+    echo -e "{Line:$index_line / Command: $fixed_command_input $line_input\n$task}\n" | tee -a $log) &
 }
 function script_silent(){       #Silence mode.
-    ((index_line++))
-    echo -e "Line:$index_line\nCommand: $fixed_command_input $line_input\n$($fixed_command_input $line_input 2>&1)\n" >> $tmp_log/$name_file.txt &
+    (task=$($fixed_command_input $line_input 2>&1)
+    echo -e "{Line:$index_line / Command: $fixed_command_input $line_input\n$task}\n" >> $log) &
 }
 
 function tentacles(){
@@ -39,20 +50,17 @@ function tentacles(){
     export file_input           #Shows the file current.
 
     #Test if the process will be silent.
-    if [[ "$script" = "script_silent" ]]; then
-        progress="progress_silent"
-    else
+    if [[ "$verbose" = true ]]; then
         progress=""
-    fi
-    #Tests if the log folder exists, if it doesn't exist it creates it.
-    if [[ ! -d "$tmp_log" ]]; then
-        mkdir -p $tmp_log
+        script="script_verbose"
+    else
+        progress="progress"
+        script="script_silent"
     fi
     #Start program with all passed parameters.
     for file_input in "${list_of_files_input[@]}"; do
         index_line=0
         total_lines=$(wc --lines < $file_input)
-        log
         echo -e "\n$fixed_command_input $file_input"
         #Read the lines of the current file one by one and start.
         while IFS= read -r line_input; do
@@ -64,10 +72,14 @@ function tentacles(){
                 [[ "$index_line" -eq "$total_lines" && "$active_parallel" -eq "0" ]] && break;
                 #Starts with the parallel limit 0.
                 if [[ "$index_line" -lt "$total_lines" && "0" -eq "$limit_parallel" ]]; then
+                    ((index_line++))
+                    create_log
                     $script
                     [[ "$total_lines" -ne "$index_line" ]] && break;
                 #Starts with parallel limit.
                 elif [[ "$index_line" -lt "$total_lines" && "$active_parallel" -lt "$limit_parallel" ]]; then
+                    ((index_line++))
+                    create_log
                     $script
                     [[ "$total_lines" -ne "$index_line" ]] && break;
                 fi
@@ -77,6 +89,6 @@ function tentacles(){
         echo -e "    Concluded !!!"
     done
     #A completion alert sounds and exits.
-    echo -e "\n     # Logs created in path $tmp_log.\n\a"
+    echo -e "\n     # Logs created in path $log_path/.\n\a"
     exit 0
 }
